ffc5015192928e88557e7416ff946e5914f60673
Visser <lr47an@INSIM.BIZ>
2016-12-01 22:50:55 +0100

    whatever

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,9 @@
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(Mapping);
+  deployer.deploy(Dummy).then(function(){
+    return deployer.deploy(Refugee, Dummy.address);
+  });
   deployer.deploy(Refugee);
   deployer.autolink();
 };
